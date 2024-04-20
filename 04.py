33f8a22872f0000b5d4e1e0b4fcb18017255a6ec
tw30006 <original.heart.2016@gmail.com>
2024-04-20 16:12:25 +0800

    04題

---
+++ 04.py
@@ -0,0 +1,35 @@
+# 編號：CANDY-004
+# 程式語言：JavaScript
+# 題目：完成函數的內容，把傳進去的秒數變成平常人類看的懂的時間格式
+
+import math
+
+
+def human_readable_timer(seconds):
+    # 實作在這裡
+    sec = math.floor(seconds % 60)
+    min = math.floor((seconds / 60) % 60)
+    hour = math.floor(seconds / 3600)
+    return f"{hour:02}:{min:02}:{sec:02}"
+
+
+print(human_readable_timer(0))
+# 印出 00:00:00
+print(human_readable_timer(59))
+# 印出 00:00:59
+print(human_readable_timer(60))
+# 印出 00:01:00
+print(human_readable_timer(90))
+# 印出 00:01:30
+print(human_readable_timer(3599))
+# 印出 00:59:59
+print(human_readable_timer(3600))
+# 印出 01:00:00
+print(human_readable_timer(45296))
+# 印出 12:34:56
+print(human_readable_timer(86399))
+# 印出 23:59:59
+print(human_readable_timer(86400))
+# 印出 24:00:00
+print(human_readable_timer(359999))
+# 印出 99:59:59
